90a2640f5e7a3bf0211481372e1d0a8d010001d1
Steven Vitali <svitali@seas.upenn.edu>
2015-09-28 21:44:48 -0400

    Add rudimentary AppStore prototype

--- src/app-store.js
+++ src/app-store.js
@@ -1,6 +1,28 @@
 
+/**
+ * Initialize an AppStore without any models
+ */
 var AppStore = function() {
+  this.modelHash = {};
+  this.COLLECTIONS = {};
+  this.ENDPOINT_NAMES = {};
+  // The root react node for the app
+  this.rootNode = null;
+  // Will contain the appStore (this)
+  // and a ref to the root data objects
+  this.rootProps = {};
+};
 
+/**
+ * Register a Backbone model for the app store
+ * @param {String} name       The name to use for the model
+ * @param {Object} collection The Backbone collection for the model
+ * @param {String} endpoint   The base URL for CRUD operations on this model
+ */
+AppStore.prototype.addModel = function(name, collection, endpoint) {
+  this.modelHash[name] = this.modelHash[name] || {};
+  this.COLLECTIONS[name] = this.COLLECTIONS[name] || collection;
+  this.ENDPOINTS[name] = this.ENDPOINTS[name] || endpoint;
 };
 
 module.exports = AppStore;
